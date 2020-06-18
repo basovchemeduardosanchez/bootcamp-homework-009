c19b5a827e0255e0d61848e0698f85ad2f9531ca
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2020-06-18 04:36:41 -0500

    Several updates - Update default answers - Added file writing code - Added comments - Added filename argument/option

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const inquirer = require("inquirer");
+// Imported to write the file
+const fs = require( 'fs' );
+const inquirer = require( 'inquirer' );
+const generateMarkdown = require( './utils/generateMarkdown.js' );
 
 function askInquirerBadgeQuestions( pCurrentInquirerAnswers ){
     return pCurrentInquirerAnswers.createBadge;
 }
 
+// SECTION Questions to ask
 const gQuestions = [
     // See inquirer question options documentation at
     // https://github.com/SBoudrias/Inquirer.js#question
 
     {
         type: "input",
-        name: "projectTitle",
+        name: "title",
         message: "What's your project title?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         default: "My New Project"
     },
     // SECTION shields.io Badge Questions
+    // ANCHOR: Generating a badge from shields.io
     // In the documentation at http://shields.io, you can create your own badge
     // using an url like the following:
     //
@@ -71,60 +76,61 @@ const gQuestions = [
     // !SECTION shields.io Badge Questions
     {
         type: "input",
-        name: "projectDescription",
+        name: "description",
         message: "What's the project's description?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         // This will appear as a task in the README.md
-        default: "- [ ] TODO"
+        default: "- [ ] Describe the project"
     },
     // SECTION Document Sections
     {
         type: "input",
-        name: "projectInstallation",
+        name: "installation",
         message: "How can the project be installed?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         // This will appear as a task in the README.md
-        default: "- [ ] TODO"
+        default: "- [ ] Describe the installation process"
     },
     {
         type: "input",
-        name: "projectUsage",
+        name: "usage",
         message: "How can someone use the project?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         // This will appear as a task in the README.md
-        default: "- [ ] TODO"
+        default: "- [ ] Describe how to use the project"
     },
     {
         type: "input",
-        name: "projectLicense",
+        name: "license",
         message: "What is the project's license?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         // This will appear as a task in the README.md
-        default: "- [ ] TODO"
+        default: "- [ ] Set the project's license"
     },
     {
         type: "input",
-        name: "projectContributing",
+        name: "contributing",
         message: "How can others contribute to this project?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         // This will appear as a task in the README.md
-        default: "- [ ] TODO"
+        default: "- [ ] Describe how to contribute"
     },
     {
         type: "input",
-        name: "projectTests",
+        name: "tests",
         message: "How can someone run the tests for this project?",
         // The default value if none is provided, it will appear in gray next to
         // the question
         // This will appear as a task in the README.md
-        default: "- [ ] TODO"
+        default: "- [ ] Describe how to run the tests"
     },
     // The two questions below will be used to generate the Questions section
+    // ANCHOR: Obtain GitHub Avatars from Username
     // According to https://stackoverflow.com/a/36380674, the user's avatar
     // image can be obtained from their GitHub username using their GitHub
     // username as seen below
@@ -135,26 +141,54 @@ const gQuestions = [
     // should be an integer
     {
         type: "input",
-        name: "projectGithubUsername",
+        name: "githubUsername",
         message: "What's your GitHub username?"
     },
     {
         type: "input",
-        name: "projectGithubEmail",
+        name: "githubEmail",
         message: "What's your GitHub account's e-mail?"
     }
     // !SECTION Document Sections
 ];
+// !SECTION Questions to ask
 
+// SECTION Write File
 function writeToFile( pFileName, pData ) {
+    fs.writeFile( pFileName, pData, 'utf8', function( pError ){
+        if( pError ){
+            throw pError;
+        } else {
+            console.log( `File "${ pFileName }" written successfully` );
+        }
+    } );
 }
+// !SECTION Write File
 
+// SECTION Main Program Function
 function init() {
     inquirer
+        // Start the inquiry
         .prompt( gQuestions )
+        // When the inquiry is done...
         .then( function( pValue ){
-            console.log( pValue );
+            // argv[ 0 ] is always node
+            // argv[ 1 ] is the script's absolute path
+            // argv[ 2 ] is the script's first argument/option
+            var lFileName = process.argv[ 2 ];
+
+            // If no name was given in the first argument then default to
+            // README.md
+            if ( !lFileName ){
+                lFileName = 'README.md';
+            }
+
+            // !! Code to generate the markdown string is located at
+            // !! ./utils/generateMarkdown.js
+            writeToFile( lFileName, generateMarkdown( pValue ) );
         } );
 }
+// !SECTION Main Program Function
 
+// ANCHOR: Program Execution
 init();
